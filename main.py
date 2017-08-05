f2277d5fa30e3062c236779caf0994921496fcb9
servingshoes <info@servingshoes.com>
2017-08-05 20:38:24 +0300

    Support Multiple Config Files for different Threads

--- main.py
+++ main.py
@@ -3,12 +3,20 @@
 # API technique from Luke Davis (@R8T3D)
 # See https://github.com/R8T3D/MeshAPI
 from classes.mesh import Mesh
-
+import os
 
 def main():
     """ Main method keeps track of the execution process. Use to control poll times and how the product list builds"""
-    m = Mesh('../config.json', 'thread-1')
-    m.start()
+    threads=[]
+    i=0
+    for config in os.listdir('configs'):
+        print config
+        if config!="config.example.json":
+            threads.append(Mesh("configs/"+config, 'thread-'+str(i)))
+            threads[i].start()
+            i+=1
+        else:
+            print ("Change the default config filename")
 
 if __name__ == '__main__':
     main()
